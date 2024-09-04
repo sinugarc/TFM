3ad4be1219a1c703a333b55997fb20c7a516b3bd
sinugarc <115789430+sinugarc@users.noreply.github.com>
2024-09-04 19:48:26 +0100

    Deleted folders

--- Results/QCRMut/MemoryExp/.ipynb_checkpoints/memoryExp-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-python3 "memoryExp.py";
-for f in "adaptPath"/*.py; do
-	python3 "$f" 
-done
--- Results/QCRMut/QCRMut/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,3 +0,0 @@
-from .QCRMut import mutant_generator
-from .basicFunctions import input_generator
-from .mutantExecution import parallelExecution
