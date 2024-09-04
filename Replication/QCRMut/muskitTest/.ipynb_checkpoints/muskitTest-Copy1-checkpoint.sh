febabd1eb4871a48e56224c388def6e3a404b5b9
sinugarc <115789430+sinugarc@users.noreply.github.com>
2024-09-04 10:06:45 +0100

    Deleting unnecesary folders

--- Replication/QCRMut/muskitTest/.ipynb_checkpoints/muskitTest-Copy1-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-QA="CE";QA="${QA} IQFT"
-for k in $QA; do
-    cp "Results/${k}/Results_100Rep/0_results.txt" "Results/${k}_100Rep";
-    cp "Results/${k}/Results_1024Rep/0_results.txt" "Results/${k}_1024Rep";
-done;
